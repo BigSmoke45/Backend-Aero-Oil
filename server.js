d4de4e5275c8447080798f1eb3daf8007c7138a6
Mark <reserv6420@icloud.com>
2026-04-11 00:16:38 +0300

    Add Firebase config endpoint to server.js
    Added Firebase configuration endpoint to server.

--- server.js
+++ server.js
@@ -12,6 +12,19 @@ app.get("/", (req, res) => {
   res.send("Aero-Oil API is running");
 });
 
+//firebase БД
+app.get('/firebase-config', (req, res) => {
+  res.json({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DB_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE,
+    messagingSenderId: process.env.FIREBASE_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+  });
+});
+
 // баланс TON
 app.get("/balance/:address", async (req, res) => {
   try {
